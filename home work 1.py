41c26108c61d07596c863154ca4743d8ecb75fb2
emans23 <iamanze79@gmail.com>
2026-01-28 18:51:01 +0000

    Add files via upload

---
+++ home work 1.py
@@ -0,0 +1,10 @@
+abi="abi birthday is 12/5/2010"
+thimoty="thimoty birthday is 21/8/2011"
+sofia= "sofia birhtday is 12/7/2013"
+jeff=  "jeff bithday is 3/2/2010"
+micheal=  "micheal birthday is 9/3/2015"
+print(abi)
+print(thimoty)
+print(sofia)
+print(jeff)
+print(micheal)
