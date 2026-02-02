9ef2b07d9c390b119a71baab779b367ffedc1da8
emans23 <iamanze79@gmail.com>
2026-02-02 17:48:46 +0000

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,8 @@
+maths=int(input("enter your marks in maths"))
+english=int(input("enter your marks in english"))
+physics=int(input("enter your marks in physics"))
+chemistry=int(input("enter your marhs in chemistry"))
+om=(maths+english+physics+chemistry)
+tm=400
+perc=(om/tm)*100
+print(perc)
