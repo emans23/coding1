930ec096d7a8885a43c5a9c1c03d8b140ede7545
emans23 <iamanze79@gmail.com>
2026-02-07 11:51:22 +0000

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,13 @@
+a=20
+b=10
+c=0
+if a and b and c:
+    print("all the  variable has boolean true")
+else:
+    print("ether one of them is false ")    
+
+x=40
+h=60
+v=-6
+if x<30 or h<30 or v<30:
+     print("atleast one of them is less than 30")
