a102b04ad002fb6c2c0fd3f60e1b818cfbd3c6d7
emans23 <iamanze79@gmail.com>
2026-02-09 17:23:04 +0000

    Add files via upload

---
+++ 18.py
@@ -0,0 +1,10 @@
+x=5
+if(type(x)is int):
+    print ("the number is an intiger")
+else:
+    print("it is not an intiger")
+f="water"
+if(type(f)is not str):
+    print ("f is not a string")
+else:
+    print("f is a string")        
