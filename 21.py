46e239809c57c7a360d9e01a962fe72f06b4be37
emans23 <iamanze79@gmail.com>
2026-02-16 17:23:11 +0000

    Add files via upload

---
+++ 21.py
@@ -0,0 +1,15 @@
+a=12
+f=4
+q=5
+b=2
+w=(a+f)*q/b
+print("the value of (a+f)*q/b is",w)
+
+name="andrew"
+age=4
+if name== "andrew"or name=="tom"and age<=4:
+    print("welcome")
+else:
+    print("goodbye")    
+
+
