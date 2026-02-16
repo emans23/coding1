c51c04704d89f82503f4e9cf4b40bf4f9181277e
emans23 <iamanze79@gmail.com>
2026-02-16 17:51:37 +0000

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,8 @@
+mean1=50
+wrong_number=47
+correct_number=43
+totalnumber=70
+sum1=mean1*totalnumber
+print("the sum is",sum1)
+sum2=sum1-((wrong_number)-(correct_number))
+print("the new mean is",sum2/totalnumber)
