8e4494819116243a52b456f2c182de4678b401f3
emans23 <iamanze79@gmail.com>
2026-03-02 17:22:44 +0000

    Add files via upload

---
+++ 38.py
@@ -0,0 +1,7 @@
+def whether_conditions():
+    print("the whether pleasent in ",spring)
+    print("the whether is the same in",autumn)
+spring="autumn"
+autumn=spring    
+whether_conditions()
+    
