d2b32c01b25ad1c83dfdc2be0a84efee4b17797a
emans23 <iamanze79@gmail.com>
2026-03-04 16:41:37 +0000

    Add files via upload

---
+++ homework13.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(400,300)
+square= turtle.Turtle()
+for i in range(4):
+    square.forward(100)
+    square.right(90)
+turtle.done()    
