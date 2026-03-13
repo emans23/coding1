b1a95decec2bac66c8cd2a86871db739815f563e
emans23 <iamanze79@gmail.com>
2026-03-13 17:20:56 +0000

    Add files via upload

---
+++ 49.py
@@ -0,0 +1,6 @@
+from datetime import date,time,datetime
+today=date.today()
+now=datetime.now()
+print("todays date is",today)
+print("current date and time is",now)
+print("date componets are",today.day,today.month,today.year)
