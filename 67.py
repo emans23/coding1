5e4884b70f8d4fb832991faafd2bc7b07a2bdd11
emans23 <iamanze79@gmail.com>
2026-04-08 17:23:24 +0100

    Add files via upload

---
+++ 67.py
@@ -0,0 +1,7 @@
+class vehicle:
+    def __init__(self,max_speed,milage):
+        self.max_speed=max_speed
+        self.milage=milage
+ob=vehicle(140,230)
+print("max speed is",ob.max_speed)
+print("milage is",ob.milage)        
