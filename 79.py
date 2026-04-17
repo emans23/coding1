eaeef05a9062acce596e4f7eb07766ffd2464114
emans23 <iamanze79@gmail.com>
2026-04-17 17:27:33 +0100

    Add files via upload

---
+++ 79.py
@@ -0,0 +1,10 @@
+class myClass:
+    __privateVar=27
+    def __privMeth(self):
+        print("I'm inside class myClass")
+    def hello(self):
+        print("private variable", myClass.__privateVar)
+foo=myClass()
+foo.hello()
+foo.__privMeth
+
