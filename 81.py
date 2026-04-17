a99fddf6a53c81beef7219360bcb9abc74f5a782
emans23 <iamanze79@gmail.com>
2026-04-17 17:45:27 +0100

    Add files via upload

---
+++ 81.py
@@ -0,0 +1,13 @@
+class Computer:
+    def __init__(self):
+        self.__maxprice=900
+    def sell(self):
+        print("selling price is",self.__maxprice)    
+    def  setmaxprice(self,price):
+        self.__maxprice=price
+c=Computer()
+c.sell()
+c.__maxprice=1000
+c.sell()
+c.setmaxprice(1000)
+c.sell()        
