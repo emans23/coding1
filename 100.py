7737804d049b30ff18a5922654475a1f16c1e18e
emans23 <iamanze79@gmail.com>
2026-07-06 17:56:55 +0100

    Add files via upload

---
+++ 100.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from datetime import date
+root=Tk()
+root.title('getting started with widgets')
+root.geometry('400x300')
+lbl=Label(text="Hey There",fg="white",bg="#072F5F", height=1,width=300)
+name_lbl=Label(text="Full name",bg="#3895D3")
+name_entry=Entry()
+def display():
+    name=name_entry.get()
+    global Message
+    Message="welcome to the application \nToday's date is"
+    greet="hello"+name+"\n"
+    text_box.insert(END,greet)
+    text_box.insert(END,Message)
+    text_box.insert(END,date.today())
+text_box=Text(height=3)
+btn=Button(text="begin",command=display,height=1,bg="#1261A0",fg='white')
+lbl.pack()
+name_lbl.pack()
+btn.pack()
+text_box.pack()
+root.mainloop()
+
+
+    
